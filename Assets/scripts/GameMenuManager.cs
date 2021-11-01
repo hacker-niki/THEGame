09e257c24f20a487d21e5614c1c72566d8a30285
hacker-niki <nktsenko@gmail.com>
2021-11-01 21:14:04 +0300

    Game menu
    Сделал внутриигровое меню

---
+++ Assets/scripts/GameMenuManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMenuManager : MonoBehaviour
+{
+    public void BackToMenu(){
+        SceneManager.LoadScene(0);
+    }
+    public void Restart() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
